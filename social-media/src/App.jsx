276f406691f84c8c5867442ae6424ffa0ed186e8
Ramnath <ramnath2544@gmail.com>
2025-08-02 13:49:40 +0530

    Make a Header, Sidebar and Footer

--- social-media/src/App.jsx
+++ social-media/src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import CreatePost from "./components/CreatePost.jsx";
+import PostList from "./components/PostList.jsx";
 
 function App() {
   return (
-    <>
-      <div>Placeholder</div>
-    </>
-  )
+    <div className="app-container">
+      <Sidebar></Sidebar>
+      <div className="content">
+        <Header></Header>
+        <CreatePost></CreatePost>
+        <PostList></PostList>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
